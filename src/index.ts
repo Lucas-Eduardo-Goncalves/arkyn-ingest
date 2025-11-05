2b2ca4bfaa5d418ffa269f469655e9cf60672d0e
Lucas Gonçalves <lucasgoncalvesgithub@gmail.com>
2025-11-05 01:44:33 -0300

    fix(index): enhance health-check endpoint response
    - Added the service name to the health-check response message.
    - Updated the response format to include the container hostname and version.
    - Improved clarity of the health-check endpoint by providing more context.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Hono } from "hono";
 import { hostname } from "os";
-import { version } from "../package.json";
+import { version, name } from "../package.json";
 
 import { environmentVariables } from "./main/config/environmentVariables";
 import { RouteLogMiddleware } from "./main/middlewares/routeLogMiddleware";
@@ -16,9 +16,10 @@ cacheDb.connect();
 
 app.use("*", (c, next) => RouteLogMiddleware.logRoute(c, next));
 
-app.get("/health-check", (c) =>
-  c.text(`${hostname()} - Service is healthy on version ${version}`)
-);
+app.get("/health-check", (c) => {
+  const message = `Service ${name} is healthy on container ${hostname()} using version ${version}`;
+  return c.text(message);
+});
 
 app.post("/ingest-log", async (c) => adaptRoute(c, sendIngestLog.handle));
 
